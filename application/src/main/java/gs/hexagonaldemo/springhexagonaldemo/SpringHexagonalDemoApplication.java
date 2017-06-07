3bc6b545b1ef17aa86cff0928c4c95042086c9ca
gshaw-pivotal <gshaw+github@pivotal.io>
2017-06-07 16:24:47 -0400

    Add application module where the spring-boot app starts

---
+++ application/src/main/java/gs/hexagonaldemo/springhexagonaldemo/SpringHexagonalDemoApplication.java
@@ -0,0 +1,12 @@
+package gs.hexagonaldemo.springhexagonaldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHexagonalDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHexagonalDemoApplication.class, args);
+	}
+}
